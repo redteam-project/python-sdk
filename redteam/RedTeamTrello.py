5a2025be1731975b341ff298117a17f1927278c9
Jason Callaway <jason@jasoncallaway.com>
2017-10-14 17:12:34 -0400

    tweaking manifest

--- redteam/RedTeamTrello.py
+++ redteam/RedTeamTrello.py
@@ -10,7 +10,7 @@ from jinja2 import Environment, FileSystemLoader
 __author__ = 'Jason Callaway'
 __email__ = 'jasoncallaway@fedoraproject.org'
 __license__ = 'GNU Public License v2'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __status__ = 'alpha'
 
 
