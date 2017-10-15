a3b56d11d6528510595f6310b8010468244909d5
Jason Callaway <jason@jasoncallaway.com>
2017-10-14 21:06:33 -0400

    still trying to get the manifest includes to work

--- redteam/IncludeFuncs.py
+++ redteam/IncludeFuncs.py
@@ -8,7 +8,7 @@ import yaml
 __author__ = 'Jason Callaway'
 __email__ = 'jasoncallaway@fedoraproject.org'
 __license__ = 'GNU Public License v2'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __status__ = 'alpha'
 
 
